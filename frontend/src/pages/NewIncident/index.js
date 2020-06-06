ae736c7b006e47c69d85827dde31e83fd94ca2af
kavinsky-spy <julio@zoocha.com>
2020-06-06 19:20:45 -0300

    HERO-02: connecting FE to api

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import api from '../../services/api';
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
+
+
 function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+   async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+
+            history.push('/');
+        } catch (error) {
+            alert('Error registring case, try again')
+        }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -23,15 +55,22 @@ function NewIncident() {
 
                 </section>
 
-                <form>
-                    <input placeholder="Cases title" />
-                    <textarea type="email" placeholder="Description" />
-                    <input placeholder="Ammount in reals" />
-
-                    <div className="input-group">
-                        <input placeholder="City" />
-                        <input placeholder="UF" style={{ width: 80 }} />
-                    </div>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Cases title" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea type="email" 
+                        placeholder="Description" 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input 
+                        placeholder="Ammount in reals" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Sign Up</button>
                 </form>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,81 +1,83 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 function Profile() {
+    // 1h 28m
+
+    const [incidents, setIncidents] = useState([]);
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+
+    const history = useHistory();
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        });
+    }, [ongId]);
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`,{
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+            setIncidents(incidents.filter(incident => incident.id !== id));
+        } catch (error) {
+            alert('Error deleting case.')
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
+
+
     return(
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
-                <span>Welcome, APAD</span>
+                <span>Welcome, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Registry new case</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
 
             <h1>Registred Cases</h1>
             <ul>
-                <li>
+                {incidents.map(incident =>(
+                    <li key={incident.id}>
                     <strong>CASE:</strong>
-                    <p>Test case</p>
+                    <p>{incident.title}</p>
 
                     <strong>DESCRIPTION:</strong>
-                    <p>Description test</p>
+                <p>{incident.description}</p>
 
                     <strong>VALUE:</strong>
-                    <p>$ 120,00</p>
+                    <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-                    <button type="button">
+                    <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                         <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
                     </button>
                 </li>
-                <li>
-                    <strong>CASE:</strong>
-                    <p>Test case</p>
-
-                    <strong>DESCRIPTION:</strong>
-                    <p>Description test</p>
-
-                    <strong>VALUE:</strong>
-                    <p>$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASE:</strong>
-                    <p>Test case</p>
-
-                    <strong>DESCRIPTION:</strong>
-                    <p>Description test</p>
-
-                    <strong>VALUE:</strong>
-                    <p>$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASE:</strong>
-                    <p>Test case</p>
-
-                    <strong>DESCRIPTION:</strong>
-                    <p>Description test</p>
-
-                    <strong>VALUE:</strong>
-                    <p>$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-                </li>
+                ))}
+            
             </ul>
         </div>
     )
